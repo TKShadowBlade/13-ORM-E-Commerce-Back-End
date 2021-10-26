c702163cc2e3ab4c2c2ab8d64a25e65b79a655c3
TKShadowBlade <katanablack85@gmail.com>
2021-10-26 02:25:17 -0400

    category DELETE route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -69,6 +69,17 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy ({
+    where: {
+      id: req.params.id
+    }
+  }).then(categoryData => {
+    if(!categoryData) {
+      res.status(404).json({message: 'No category under this ID'});
+      return;
+    }
+    res.json(categoryData);
+  }).catch(err => res.status(500).json(err))
 });
 
 module.exports = router;
