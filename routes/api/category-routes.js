96db16a1f5f78ac6d86b6cdd0ba29ab2b9e35d1b
TKShadowBlade <katanablack85@gmail.com>
2021-10-26 02:20:07 -0400

    category POST and PUT routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -45,10 +45,26 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create ({
+    category_name: req.body.category_name
+  })
+  .then(categoryData => res.json(categoryData))
+  .catch(err => res.status(500).json(err));
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  }).then(categoryData => {
+    if (!categoryData[0]) {
+      res.status(404).json({message: 'No category under this ID'});
+      return;
+    }
+    res.json(categoryData);
+  }).catch(err => res.status(500).json(err));
 });
 
 router.delete('/:id', (req, res) => {
